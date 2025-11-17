4726b4ee45da2067ddcaa6b21e48938cd0cf0723
francesco.miliotti <francesco.miliotti@email.com>
2025-11-17 18:02:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const bici = [
+
+{
+    "nome" : "mbt",
+    "peso" : 20
+
+},
+
+{
+
+    "nome" : "bmx",
+    "peso" : 15
+},
+{
+
+    "nome" : "gravel",
+    "peso" : 10
+}
+
+
+];
+
+console.log(bici);
