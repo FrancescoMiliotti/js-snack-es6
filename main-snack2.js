2e4b6af193e34d9d145789a51b628ba612bf8364
francesco.miliotti <francesco.miliotti@email.com>
2025-11-19 18:09:37 +0100

    for+getRandomNumber

---
+++ main-snack2.js
@@ -0,0 +1,31 @@
+
+
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const teams = [
+
+    { name: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+
+    { name: "Milan", puntiFatti: 0, falliSubiti: 0 },
+
+    { name: "Inter", puntiFatti: 0, falliSubiti: 0 },
+
+    { name: "Roma", puntiFatti: 0, falliSubiti: 0 }
+
+
+];
+
+console.log(teams)
+
+
+for (let i = 0; i < teams.length; i++) {
+    const team = teams [i];
+
+ 
+
+    team.puntiFatti = getRandomNumber(50, 100);
+    team.falliSubiti = getRandomNumber(20 , 70);
+
+    console.log(i, team);
+}
