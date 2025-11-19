7be5150915b47a15c0dc8022ae014e98ca00cd07
francesco.miliotti <francesco.miliotti@email.com>
2025-11-19 17:54:46 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 const bikelist = [
 
-    { name: "mbt", weight: 20},
+    { name: "mbt", weight: 20 },
 
-    { name: "bmx", weight: 15},
-    
-    { name: "gravel", weight: 10},
-    
-    { name: "corsa", weight: 8}
+    { name: "bmx", weight: 25 },
+
+    { name: "gravel", weight: 15 },
+
+    { name: "corsa", weight: 8 }
 
 
 ];
 
 
-
-    
+let bikeLighter = bikelist[0];
 
 for (let i = 0; i < bikelist.length; i++) {
     const bike = bikelist[i];
-    console.log("bike: ", bike)
-    const bikeWeight = bike.weight
-    console.log("weight ", bikeWeight);
+    
+    const bikeWeight = bike.weight;
+    
 
 
+    if (bikeWeight < bikeLighter.weight) {
+        bikeLighter = bike;
+    }
 
-
-
-
+    console.log(i,bike);
 
 }
 
+console.log("La Bici più leggera è :", bikeLighter);
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
